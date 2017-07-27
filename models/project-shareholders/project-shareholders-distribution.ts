fc95446e12117b14487e6e640d8d712423e3ae20
Mathew <mathew.corm@gmail.com>
2017-07-27 11:00:02 -0400

    Add ProjectShareholdersSnapshot members

--- models/project-shareholders/project-shareholders-distribution.ts
+++ models/project-shareholders/project-shareholders-distribution.ts
@@ -5,14 +5,17 @@ import { ShareholderDescription } from './shareholder-description'
  * Contains the basic share distribution for a specific project.
  * May contain Exposito users and/or github projects.
  * 
- * Since lines of code for projects varies over time, therefor
- * the shares distribution is not fixed either. See ProjectShareholdersSnapshot
+ * Since lines of code for projects varies over time, the shares 
+ * distribution is not fixed either. See {@link ProjectShareholdersSnapshot}
  * for project shareholders distribution at a specific moment in time.
  */
 export class ProjectShareholdersDistribution {
 
     id: string
 
+    /**
+     * Exposito project's Id
+     */
     @ObjectId
     projectId: string
 
--- models/project-shareholders/project-shareholders-snapshot.ts
+++ models/project-shareholders/project-shareholders-snapshot.ts
@@ -1,3 +1,32 @@
+ import { ObjectId } from '../../lib/objectid'
+ import { Shareholder } from './shareholder'
+ 
+ /** 
+  * Since lines of code for projects varies over time, the shares
+  * distribution is not fixed either. A ProjectShareholdersSnapshot
+  * represents a project shareholders distribution at a specific moment 
+  * in time.
+  */
 export class ProjectShareholdersSnapshot {
     
+    id: string
+
+    /**
+     * Exposito project's Id
+     */
+    @ObjectId
+    projectId: string
+
+    /**
+     * Moment of the snapshot
+     */
+    date: Date
+
+    /**
+     * All shareholders of the project
+     * at the specific moment, with their
+     * respective number of shares
+     */
+    shareholders: Shareholder[]
+
 }
